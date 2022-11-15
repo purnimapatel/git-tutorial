b02eb45f257ff315149c4b909b19f371bb79bfcf
Anjali Patel <24septanjali@gmail.com>
2022-11-15 14:32:42 +0530

    added file3.js

---
+++ file3.js
@@ -0,0 +1,9 @@
+let person = {
+    firstName: 'Akash',
+    lastName:'Kumar',
+    //Method
+    getFunction: function(){
+        return (`The Name of the Person is ${person.firstName} ${person.lastName}.` )
+    }
+}
+console.log(person.getFunction());
